388437d6838657dfb43f7c09376db97c3a2790d4
Bootez <8ootez@gmail.com>
2014-08-22 16:04:43 +0800

    DialogThem demo

---
+++ src/com/example/dialogthem/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dialogthem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        button = (Button)findViewById(R.id.btn_button);
+        button.setOnClickListener(new buttonListener());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    class buttonListener implements OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            finish();
+        };
+    }
+
+}
